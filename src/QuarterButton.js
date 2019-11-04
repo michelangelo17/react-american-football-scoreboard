2cc8a5124dbcf4d790e9f3d623af812be38e5726
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-04 13:01:02 -0800

    added untracked component, 'QuarterButton'

---
+++ src/QuarterButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const QuarterButton = props => {
+  const quarterHandler = () => {
+    props.quarter === 4 ?
+    props.setQuarter(1) :
+    props.setQuarter(props.quarter + 1)
+  }
+  return (
+    <div className="buttons">
+    <button 
+      onClick={() => quarterHandler()}
+      className="quarterButton"
+    >
+      Change Quarter
+    </button>
+    </div>
+  )
+}
+
+export default QuarterButton;
